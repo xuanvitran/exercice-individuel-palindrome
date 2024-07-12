5ab1cb479493d2b948a142539e6d1845db97676d
xuanvitran <xuan-vi@worldcandance.com>
2024-07-12 16:48:46 +0200

    add step1

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-// coucou
+const maxDaysInMonth = (mois) => {
+  const joursDansMois = {
+    1: 31,
+    2: 29,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31,
+  }
+
+  return joursDansMois[mois];
+}
+
+function isValidDate(date) {
+  const dateFormatTab = date.split("/");
+  const jour = dateFormatTab[0];
+  const mois = dateFormatTab[1];
+  const annee = dateFormatTab[2];
+
+  // if ((annee > 999 && annee <= 9999) 
+  //     && (mois >= 1 && mois <= 12) 
+  //     && (jour >= 1 && jour <= maxDaysInMonth(mois))) {
+  //       return true;
+  // }
+
+  // return false;
+
+  if (annee > 999 && annee <= 9999) {
+    // annee valide
+    if (mois >= 1 && mois <= 12) {
+      // mois valide
+      if (jour >= 1 && jour <= maxDaysInMonth(parseInt(mois))) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log('result:', isValidDate("30/06/2024"));
+// isValidDate("03/14/2001");
